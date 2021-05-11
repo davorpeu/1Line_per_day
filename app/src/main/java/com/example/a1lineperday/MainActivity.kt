af379b3aa5ede632001d5bed9b7ec5c6c5584b44
dadop <davorpeu@gmail.com>
2021-05-11 09:59:36 +0200

    Another line another day

--- app/src/main/java/com/example/a1lineperday/MainActivity.kt
+++ app/src/main/java/com/example/a1lineperday/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.a1lineperday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById(R.id.btnStart) as Button
+        // set on-click listener
+        btnStart.setOnClickListener{
+            Toast.makeText(this@MainActivity, "You clicked me ye.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
