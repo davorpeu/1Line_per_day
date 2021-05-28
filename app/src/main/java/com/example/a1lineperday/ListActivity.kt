0e1a8b57fc3a76d1374d0098e09db90e189380dc
dadop <davorpeu@gmail.com>
2021-05-28 16:36:32 +0200

    Moved the arraylists to MainActivity.kt

--- app/src/main/java/com/example/a1lineperday/ListActivity.kt
+++ app/src/main/java/com/example/a1lineperday/ListActivity.kt
@@ -1,11 +1,3 @@
-package com.example.a1lineperday;
+package com.example.a1lineperday
 
-import java.util.ArrayList;
-import java.util.Date;
-
-public class ListActivity {
-    private ArrayList<String> task;
-    private ArrayList<Date> time;
-    private ArrayList<String> location;
-
-}
+class ListActivity 
--- app/src/main/java/com/example/a1lineperday/MainActivity.kt
+++ app/src/main/java/com/example/a1lineperday/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.a1lineperday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val task: ArrayList<String>? = null
+    private val time: ArrayList<Date>? = null
+    private val location: ArrayList<String>? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,4 +24,7 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
+
 }
