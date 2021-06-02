445725dfd6f27d17066c75a38f3ac665441b45ac
dadop <davorpeu@gmail.com>
2021-06-02 19:10:36 +0200

    Added a function that will add a task. Needs work

--- app/src/main/java/com/example/a1lineperday/MainActivity.kt
+++ app/src/main/java/com/example/a1lineperday/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.a1lineperday
 
+import android.R
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private val task: ArrayList<String>? = null
@@ -20,11 +24,19 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById(R.id.action_Add_Task) as Button
         // set on-click listener
         btnStart.setOnClickListener{
-            Toast.makeText(this@MainActivity, "You clicked me ye.", Toast.LENGTH_SHORT).show()
+
+            Toast.makeText(this@MainActivity, "Task Created.", Toast.LENGTH_SHORT).show()
         }
 
     }
 
+    //Solve tommorow make this add a task
+    fun onAddItem(v: View?) {
+        val etNewItem = findViewById<View>(R.id.etNewItem) as EditText
+        val itemText = etNewItem.text.toString()
+        itemsAdapter.add(itemText)
+        etNewItem.setText("")
+    }
 
 
 }
